f66e05caaf0dd55ba3e438e22a3cd36e2c604fd0
Simón Jaramillo <sjaramillo5@uc.cl>
2025-09-05 14:15:36 -0400

    Cleaner main file

--- full_provision/main/main.c
+++ full_provision/main/main.c
@@ -1,57 +1,33 @@
-/* MQTT Mutual Authentication Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
-#include <stdio.h>
-#include <stdint.h>
-#include <stddef.h>
-#include <string.h>
-#include "esp_wifi.h"
-#include "esp_system.h"
-#include "nvs_flash.h"
-#include "esp_event.h"
-#include "esp_netif.h"
-//#include "protocol_examples_common.h"
-
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/semphr.h"
-#include "freertos/queue.h"
-
-#include "lwip/sockets.h"
-#include "lwip/dns.h"
-#include "lwip/netdb.h"
-
-#include "provision_claimer.h"
-#include "WiFi_manager.h"
-
-#include "esp_log.h"
-
+/* Espressif includes */
+#include <esp_err.h>
+#include <esp_log.h>
+#include <esp_system.h>
+/* Standard includes*/
+/* FreeRTOS includes*/
+/* Custom includes */
+#include "ethernet_manager.h"
+#include "mqtt_worker.h"
 
 static const char *TAG = "full-provision";
 
 void app_main(void)
 {
-    ESP_LOGI(TAG, "[APP] Startup..");
-    ESP_LOGI(TAG, "[APP] Free memory: %" PRIu32 " bytes", esp_get_free_heap_size());
-    ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
+  ESP_LOGI(TAG, "[APP] Startup..");
+  ESP_LOGI(TAG, "[APP] Free memory: %" PRIu32 " bytes", esp_get_free_heap_size());
+  ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
 
-    esp_log_level_set("*", ESP_LOG_INFO);
-    esp_log_level_set("mqtt_client", ESP_LOG_VERBOSE);
-    esp_log_level_set("transport_base", ESP_LOG_VERBOSE);
-    esp_log_level_set("transport", ESP_LOG_VERBOSE);
-    esp_log_level_set("outbox", ESP_LOG_VERBOSE);
+  // Set log levels
+  esp_log_level_set("*", ESP_LOG_INFO);
+  esp_log_level_set("mqtt_client", ESP_LOG_VERBOSE);
+  esp_log_level_set("transport_base", ESP_LOG_VERBOSE);
+  esp_log_level_set("transport", ESP_LOG_VERBOSE);
+  esp_log_level_set("Provision Claimer", ESP_LOG_VERBOSE);
+  esp_log_level_set("MQTT Worker", ESP_LOG_VERBOSE);
+  esp_log_level_set("NVS Manager", ESP_LOG_VERBOSE);
 
-    ESP_ERROR_CHECK(nvs_flash_init());
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
+  // Initialize ethernet
+  esp_eth_handle_t eth_handle;
+  ESP_ERROR_CHECK(ethman_init(&eth_handle));
 
-    wifi_init_sta();
-    connect_wifi(CONFIG_SSID, CONFIG_PASSWORD);
-
-    provisioner_start();
+  mqttworker_begin();
 }
