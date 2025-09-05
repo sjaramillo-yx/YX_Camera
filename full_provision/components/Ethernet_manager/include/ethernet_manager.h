fa8b4ca3dca78a7d0508c748ede91defd8b54921
Simón Jaramillo <sjaramillo5@uc.cl>
2025-09-05 14:12:50 -0400

    Basic Ethernet capability

---
+++ full_provision/components/Ethernet_manager/include/ethernet_manager.h
@@ -0,0 +1,44 @@
+/**
+ * @file ethernet_manager.h
+ * @date September 2025
+ * @author Simón Jaramillo <sjaramillo@yx.cl>
+ * 
+ * @brief This header provides helper functions to create and bind the ethernet
+ * interface
+ */
+#pragma once
+
+ /* Espressif includes */
+#include <esp_check.h>
+#include <esp_err.h>
+#include <esp_eth_driver.h>
+#include <esp_eth_netif_glue.h>
+#include <esp_event.h>
+#include <esp_log.h>
+#include <esp_mac.h>
+#include <esp_netif.h>
+#include <esp_system.h>
+/* Standard includes*/
+/* FreeRTOS includes*/
+/* Custom includes */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief Initialize the Ethernet driver
+ * 
+ * @param[out] eth_handle
+ * 
+ * @return
+ *          - `ESP_OK` on success
+ *          - `ESP_ERR_INVALID_ARG` when passed invalid pointers
+ *          - `ESP_ERR_NO_MEM` when there is no memory to allocate for Ethernet driver handles array
+ *          - `ESP_FAIL` on any other failure
+ */
+esp_err_t ethman_init(esp_eth_handle_t *eth_handle_out);
+
+#ifdef __cplusplus
+}
+#endif
